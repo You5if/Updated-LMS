ea688c3ed56ea5c0961466ab20169183b1497483
albraa3adil369 <a.adil@markoncs.com>
2023-01-29 10:26:50 +0200

    server changes

--- src/app/app.global.ts
+++ src/app/app.global.ts
@@ -9,9 +9,12 @@ export class AppGlobals {
 
   
   /* Development Server */
-  readonly baseDomainUrl: string = "qualityhouse.auto-paymcs.com";
-  readonly baseAppUrl: string = "http://qualityhouse.auto-paymcs.com/";
-  readonly baseLandingAppUrl: string = "http://qualityhouse.auto-paymcs.com/";
+  // readonly baseDomainUrl: string = "qualityhouse.auto-paymcs.com";
+  readonly baseDomainUrl: string = "http://49.50.77.201/qualityhouse/";
+  // readonly baseAppUrl: string = "http://qualityhouse.auto-paymcs.com/";
+  readonly baseAppUrl: string = "http://49.50.77.201/qualityhouse/";
+  // readonly baseLandingAppUrl: string = "http://qualityhouse.auto-paymcs.com/";
+ readonly baseLandingAppUrl: string = "http://49.50.77.201/qualityhouse/";
 
   /* Live Server */
   // readonly baseDomainUrl: string = "aladdinshipping.com";
@@ -24,9 +27,12 @@ export class AppGlobals {
   //   readonly baseAPIRootUrl: string = 'https://localhost:44377/api/';
 
   /* Development Server */
-  readonly baseAPIFileUrl: string = "http://lmsapi.autopay-mcs.com/";
-  readonly baseAPIUrl: string = "http://lmsapi.autopay-mcs.com/api/";
-  readonly baseAPIRootUrl: string = "http://lmsapi.autopay-mcs.com/api/";
+  readonly baseAPIFileUrl: string = "http://49.50.77.201/lmsapi/";
+  readonly baseAPIUrl: string = "http://49.50.77.201/lmsapi/api/";
+  readonly baseAPIRootUrl: string = "http://49.50.77.201/lmsapi/api/";
+  // readonly baseAPIFileUrl: string = "http://lmsapi.autopay-mcs.com/";
+  // readonly baseAPIUrl: string = "http://lmsapi.autopay-mcs.com/api/";
+  // readonly baseAPIRootUrl: string = "http://lmsapi.autopay-mcs.com/api/";
 
   /* Live Server */
   // readonly baseAPIFileUrl: string = "https://appapi.aladdinshipping.com/";
@@ -35,8 +41,8 @@ export class AppGlobals {
 
   /* Development Server */
   // readonly baseReportUrl: string = 'http://shippingreports.autopay-mcs.com/default.aspx?';
-  readonly baseReportUrl: string = 'http://lmsreports.autopay-mcs.com/default.aspx?';
-  readonly baseReportEmailUrl: string = 'http://lmsreports.autopay-mcs.com/default2.aspx?';
+  readonly baseReportUrl: string = 'http://49.50.77.201/lmsreports/default.aspx?';
+  readonly baseReportEmailUrl: string = 'http://49.50.77.201/lmsreports/default2.aspx?';
 
   /* Live Server */
   // readonly baseReportUrl: string =
